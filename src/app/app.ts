9887d5c21fbd929dbfb5d6ca280f8e05b5a908f7
keven-bardales <keven.bardales@gmail.com>
2025-06-24 23:03:48 -0600

    feat: add angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
