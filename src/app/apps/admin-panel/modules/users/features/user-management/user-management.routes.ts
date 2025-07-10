965fe34a71e53dbd8a1eb531a69afe914256d15a
keven-bardales <keven.bardales@gmail.com>
2025-07-09 20:33:25 -0600

    fix: user list import

--- src/app/apps/admin-panel/modules/users/features/user-management/user-management.routes.ts
+++ src/app/apps/admin-panel/modules/users/features/user-management/user-management.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from "@angular/router";
 
 export const userManagementRoutes: Routes = [
   {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
     path: 'list',
     loadChildren: () => import('./pages/user-list-page/user-list-page.routes').then(m => m.userListPageRoutes)
   }
