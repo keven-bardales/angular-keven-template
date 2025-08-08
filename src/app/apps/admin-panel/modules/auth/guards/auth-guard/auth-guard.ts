f6a2efeec760b152ba426018eb8e3bfcec5de1fb
keven-bardales <keven.bardales@gmail.com>
2025-08-07 22:03:48 -0600

    feat: add package json linting command and changes in default auth guard

--- src/app/apps/admin-panel/modules/auth/guards/auth-guard/auth-guard.ts
+++ src/app/apps/admin-panel/modules/auth/guards/auth-guard/auth-guard.ts
@@ -1,10 +1,10 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
-import { AuthServiceToken } from "../../services/auth-service";
+import { IAuthService } from "../../services/auth-service/auth-service.inteface";
 import { map, take } from "rxjs";
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthServiceToken);
+  const authService = inject(IAuthService);
   const router = inject(Router);
 
   return authService.checkAuthStatus().pipe(
